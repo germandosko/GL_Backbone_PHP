ebd6f30ee20c44927161158eb9e3750500628d23
German Dosko <gdosko@vpar-gdosko.(none)>
2014-03-27 14:44:03 -0300

    files deleted

--- resources/js/views/customer.js
+++ resources/js/views/customer.js
@@ -1,4 +1,5 @@
 var CustomerView = Backbone.View.extend({
+	//el: $("#content"),
 
 	results: Handlebars.compile($('#customers_results').text()),
 	
@@ -7,37 +8,55 @@ var CustomerView = Backbone.View.extend({
 	newForm: Handlebars.compile($('#customer_newForm').text()),
 
     events: {
+        "click .toBack": "renderContent",
         "click #search": "renderResults",
-        "click #newCustomer": "formNew",
+        "click #newCustomer": "newCustForm",
         "click #saveCustomer": "save",
       	"click .deleteCustomer": "delete"          
     },
 	
 	initialize:function () {
-        _.bindAll(this);
+	    console.log('initialize');
+	    _.bindAll(this);
     },
 
 	renderContent: function () {	
+		console.log('renderContent');
 		this.$el.html(this.template());		
 		$('#content').html(this.$el);
 	},
-	
-	formNew: function (event) {	
+		
+
+	newCustForm: function (event) {	
 		event.preventDefault();
 		this.$el.html(this.newForm());		
-		$('#content').html(this.$el);
+		$('#content').html(this.$el);	
 	},
 
 	delete: function (event) {	
 		event.preventDefault();
-		
-		console.log('deleteo???');
+		var url = 'Backend/Api/customerDelete.php';
+		var customerId = {'id': event.currentTarget.parentNode.parentNode.firstElementChild.innerText};
+		event.currentTarget.parentNode.parentNode.remove();
+		that = this;
+		/*
+		$.ajax({
+            data: customerId,
+            url: url,
+            type: 'POST',
+            success: function (data) {
+                console.log(data);
+            },
+		});
+		*/
+		window.location.replace('#admin_customers');
+
 	},
 
 	save: function (event) {	
 		event.preventDefault();
-		console.log('funco?');
-		/*
+		console.log('ingreso save');
+			/*
 		var url = 'Backend/Api/customerSave.php';
 		var formValues = {
 			'cuit': $('#cuit').val(),
@@ -66,6 +85,7 @@ var CustomerView = Backbone.View.extend({
 
 	renderResults: function (event) {	
 		event.preventDefault();
+		console.log('renderResults');
 		var url = 'Backend/Api/customerSearch.php';
 		var formValues = {
 			'customerName': $('#customerName').val()
@@ -78,6 +98,9 @@ var CustomerView = Backbone.View.extend({
             success: function (data) {
                 if(data){
                 	jsonObject = JSON.parse(data);
+					console.log(that.results({"items":jsonObject}));
+					//that.$el.html(that.results({"items":jsonObject}));		
+					//$('#customers').html(that.$el);					
 					$('#customers').html(that.results({"items":jsonObject}));
                 } else {
                 	console.log('ERROR');
